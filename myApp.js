5f874a1363df1edbe6b7deaca728bcc471d43ee9
Khanti <krindani99@gmail.com>
2024-01-17 21:05:51 +0530

    Enable body-parser

--- myApp.js
+++ myApp.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('body-parser');
 let express = require('express');
 let app = express();
 
@@ -6,7 +7,8 @@ app.use("/public", express.static(__dirname + "/public"));
 app.use((req, res, next)=>{
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next();
-})
+});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.get("/json", (req, res) => res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"}));
@@ -14,5 +16,9 @@ app.get("/now", (req, res, next) => {
     req.time = new Date().toString();
     next();
 }, (req, res) => res.json({"time": req.time}));
+app.get("/:word/echo", (req, res) => res.json({"echo": req.params.word}));
+
+const nameHandler = (req, res) => res.json({"name": req.query.firstName + " " + req.query.lastName});
+app.route("/name").get(nameHandler).post(nameHandler);
 
 module.exports = app;
