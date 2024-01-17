b272913b1a20f3dbc75eaf9be036853f34e940c1
Khanti <krindani99@gmail.com>
2024-01-17 20:36:38 +0530

    Use env variables

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
-app.get("/json", (req, res) => res.json({"message": "Hello json"}));
+app.get("/json", (req, res) => res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"}));
 
 module.exports = app;
