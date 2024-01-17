ad967762d57493bf63fe059f4a8866fb7bcd4847
Khanti <krindani99@gmail.com>
2024-01-17 20:49:56 +0530

    Logger as middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let express = require('express');
 let app = express();
 
 app.use("/public", express.static(__dirname + "/public"));
+app.use((req, res, next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.get("/json", (req, res) => res.json({"message": process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"}));
