32254293b317770b91dd0f1bef0acbe728561445
Khanti <krindani99@gmail.com>
2024-01-17 20:29:44 +0530

    Set static assets path

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let app = express();
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/views/index.html");
-});
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 module.exports = app;
