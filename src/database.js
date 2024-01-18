efbb3939ca4b5983a1733617027afcba6923fb30
Khanti <krindani99@gmail.com>
2024-01-18 08:47:15 +0530

    Person object from schema

--- src/database.js
+++ src/database.js
@@ -19,10 +19,10 @@ class Database {
 
 new Database();
 
-let personSchema = new mongoose.Schema({
-    name: String,
+const personSchema = new mongoose.Schema({
+    name: { type: String, required: true },
     age: Number,
     favoriteFoods: [String]
   });
 
-module.exports = mongoose.model('Person', personSchema);
+const Person = mongoose.model("Person", personSchema);
