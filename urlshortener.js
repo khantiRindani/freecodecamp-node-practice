687e74e4335f05b6a2acb4d9ed503167f1f3df58
Khanti <krindani99@gmail.com>
2024-01-18 18:31:47 +0530

    Use URL type

--- urlshortener.js
+++ urlshortener.js
@@ -13,8 +13,8 @@ const revMap = new Map();
 counter = -1;
 
 router.post('/api/shorturl', (req, res) => {
-    url = req.body.url.replace(new RegExp("https?://"), '');
-    dns.lookup(url, (err, add) => {
+    url = req.body.url;
+    dns.lookup(new URL(url).hostname, (err, add) => {
         if(err) {
             res.json({error: "invalid url"});
         }
@@ -31,7 +31,7 @@ router.get('/api/shorturl/:shortUrl', (req, res) => {
     if(!revMap.has(Number(req.params.shortUrl))) {
         res.json({error: "invalid url"});
     }
-    res.redirect("http://" + revMap.get(Number(req.params.shortUrl)));
+    res.redirect(revMap.get(Number(req.params.shortUrl)));
 });
 
 module.exports = router;
